KDE/qqc2-desktop-style

commit 4f00b0cabc1230fdf1fa9511443dbfbc1a2f610f

Fix ComboBox
Summary: It wasn't working at all

Reviewers: #kirigami, mart

Reviewed By: #kirigami, mart

Subscribers: plasma-devel

Tags: #plasma

Differential Revision: https://phabricator.kde.org/D18189

org.kde.desktop/ComboBox.qml
@@ -65,7 +65,7 @@ T.ComboBox {
         property int indexUnderMouse: -1
         onWheel: {
             if (wheel.pixelDelta.y < 0 || wheel.angleDelta.y < 0) {
-                controlRoot.currentIndex = Math.min(controlRoot.currentIndex + 1, delegateModel.count -1);
+                controlRoot.currentIndex = Math.min(controlRoot.currentIndex + 1, model.count -1);
             } else {
                 controlRoot.currentIndex = Math.max(controlRoot.currentIndex - 1, 0);
             }
@@ -134,7 +134,7 @@ T.ComboBox {
             selectedTextColor: Kirigami.Theme.highlightedTextColor
 
             selectByMouse: !Kirigami.Settings.tabletMode
-            cursorDelegate: Kirigami.Settings.tabletMode ? mobileCursor : undefined
+            cursorDelegate: Kirigami.Settings.tabletMode ? mobileCursor : null
 
             font: controlRoot.font
             horizontalAlignment: Text.AlignLeft
@@ -202,9 +202,10 @@ T.ComboBox {
 
         contentItem: ListView {
             id: listView
-            clip: true
+
             implicitHeight: contentHeight
-            model: controlRoot.popup.visible ? controlRoot.delegateModel : null
+            model: controlRoot.model
+            delegate: controlRoot.delegate
             currentIndex: controlRoot.highlightedIndex
             highlightRangeMode: ListView.ApplyRange
             highlightMoveDuration: 0

tests/testComboBox.qml
@@ -1,0 +1,29 @@
+import QtQuick 2.3
+import QtQuick.Layouts 1.3
+import QtQuick.Controls 2.3
+
+ApplicationWindow
+{
+    visible: true
+
+    ColumnLayout {
+        anchors.fill: parent
+        ComboBox {
+            Layout.fillWidth: true
+            textRole: "key"
+            model: ListModel {
+                id: comboModel
+                ListElement { key: "First"; value: 123 }
+                ListElement { key: "Second"; value: 456 }
+                ListElement { key: "Third"; value: 789 }
+            }
+        }
+
+        ListView {
+            Layout.fillWidth: true
+            Layout.fillHeight: true
+            model: comboModel
+            delegate: Label { text: key }
+        }
+    }
+}

commit 581b140c8b81096f40ddebd84b8b2fbc327b616d

partly revert 4f00b0cabc1230fdf
4f00b0cabc1230fdf broke mousewheel switching for the combobox
the proper model to use is delegateModel
CCMAIL:alieixpol@kde.org

org.kde.desktop/ComboBox.qml
@@ -69,7 +69,7 @@ T.ComboBox {
         property int indexUnderMouse: -1
         onWheel: {
             if (wheel.pixelDelta.y < 0 || wheel.angleDelta.y < 0) {
-                controlRoot.currentIndex = Math.min(controlRoot.currentIndex + 1, model.count -1);
+                controlRoot.currentIndex = Math.min(controlRoot.currentIndex + 1, delegateModel.count -1);
             } else {
                 controlRoot.currentIndex = Math.max(controlRoot.currentIndex - 1, 0);
             }
@@ -207,7 +207,7 @@ T.ComboBox {
             id: listView
 
             implicitHeight: contentHeight
-            model: controlRoot.model
+            model: controlRoot.delegateModel
             delegate: controlRoot.delegate
             currentIndex: controlRoot.highlightedIndex
             highlightRangeMode: ListView.ApplyRange