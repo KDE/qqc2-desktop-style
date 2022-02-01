17fb030bf88e4d528de8e7954bd8ba98ef815db0
Nicolas Fella <nicolas.fella@gmx.de>
2022-02-01 13:10:57 +0100

    Use correct DBus signal for listening for font changes
    The fonts KCM doesn't use the KGlobalSettings signal.
    
    Instead it uses another signal, so connect to that

--- kirigami-plasmadesktop-integration/plasmadesktoptheme.cpp
+++ kirigami-plasmadesktop-integration/plasmadesktoptheme.cpp
@@ -38,13 +38,13 @@ public:
     {
         connect(qGuiApp, &QGuiApplication::paletteChanged, this, &StyleSingleton::refresh);
 
-        // Use DBus in order to listen for kdeglobals changes directly, as the
+        // Use DBus in order to listen for settings changes directly, as the
         // QApplication doesn't expose the font variants we're looking for,
         // namely smallFont.
         QDBusConnection::sessionBus().connect(QString(),
-                                              QStringLiteral("/KGlobalSettings"),
-                                              QStringLiteral("org.kde.KGlobalSettings"),
-                                              QStringLiteral("notifyChange"),
+                                              QStringLiteral("/KDEPlatformTheme"),
+                                              QStringLiteral("org.kde.KDEPlatformTheme"),
+                                              QStringLiteral("refreshFonts"),
                                               this,
                                               SLOT(notifyWatchersConfigurationChange()));
 
