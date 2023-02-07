KDE/qqc2-desktop-style

commit 939176f4a556ed38570db6e6735ba44a1015ea67

Do not set the palette for every component
This makes it properly display disabled checkboxes as grey.

CCBUG: 406295

org.kde.desktop/Button.qml
@@ -13,7 +13,6 @@ import org.kde.kirigami 2.4 as Kirigami
 T.Button {
     id: controlRoot
 
-    palette: Kirigami.Theme.palette
     Kirigami.Theme.colorSet: Kirigami.Theme.Button
     Kirigami.Theme.inherit: false
 

org.kde.desktop/ComboBox.qml
@@ -17,7 +17,6 @@ import "private" as Private
 
 T.ComboBox {
     id: controlRoot
-    palette: Kirigami.Theme.palette
     //NOTE: typeof necessary to not have warnings on Qt 5.7
     Kirigami.Theme.colorSet: typeof(editable) != "undefined" && editable ? Kirigami.Theme.View : Kirigami.Theme.Button
     Kirigami.Theme.inherit: false

org.kde.desktop/DelayButton.qml
@@ -13,7 +13,6 @@ import org.kde.kirigami 2.4 as Kirigami
 T.DelayButton {
     id: controlRoot
 
-    palette: Kirigami.Theme.palette
     Kirigami.Theme.colorSet: Kirigami.Theme.Button
     Kirigami.Theme.inherit: false
 

org.kde.desktop/Dial.qml
@@ -15,7 +15,6 @@ import org.kde.qqc2desktopstyle.private 1.0 as StylePrivate
 T.Dial {
     id: controlRoot
 
-    palette: Kirigami.Theme.palette
     implicitWidth: 128
     implicitHeight: 128
 

org.kde.desktop/ProgressBar.qml
@@ -14,7 +14,6 @@ import org.kde.kirigami 2.4 as Kirigami
 T.ProgressBar {
     id: controlRoot
 
-    palette: Kirigami.Theme.palette
     implicitWidth: 250
     implicitHeight: 22
 

org.kde.desktop/ScrollBar.qml
@@ -14,7 +14,6 @@ import org.kde.kirigami 2.11 as Kirigami
 T.ScrollBar {
     id: controlRoot
 
-    palette: Kirigami.Theme.palette
     implicitWidth: background.implicitWidth
     implicitHeight: background.implicitHeight
 

org.kde.desktop/SpinBox.qml
@@ -14,7 +14,6 @@ import org.kde.qqc2desktopstyle.private 1.0 as StylePrivate
 
 T.SpinBox {
     id: controlRoot
-    palette: Kirigami.Theme.palette
     Kirigami.Theme.colorSet: Kirigami.Theme.View
     Kirigami.Theme.inherit: false
 

org.kde.desktop/TabBar.qml
@@ -14,7 +14,6 @@ import QtQuick.Templates @QQC2_VERSION@ as T
 T.TabBar {
     id: controlRoot
 
-    palette: Kirigami.Theme.palette
     Kirigami.Theme.colorSet: Kirigami.Theme.Window
     Kirigami.Theme.inherit: false
 

commit 55f5eb707b17d47bd3e776f803f1656ff5ff2e47

use again the palette coming from Kirigami.Theme
It's supposed to be possible to change "color set" of controls by
setting their inherit flag to false, but at some point that got broken
in commit 939176f4a556ed3 as a fix to bug 406295

but this caused bug 465054

bring partially back the palette binding, but for most controls, only
when inherit of their attached theme property is true (which is not
default) as the application of color is not perfect (this is due
to incompatibilities between QPalette api and the horrible KColorScheme
api, wonder if it can be corrected in kf6 to play a bit nicer)

checkboxes still not apply the palette, so there is no risk of 406295
happening again and in general by default everything doesn't look with
altered colors, but one has to "want" it by setting inherit

BUG:465054

org.kde.desktop/Button.qml
@@ -14,6 +14,7 @@ import org.kde.kirigami 2.4 as Kirigami
 T.Button {
     id: controlRoot
 
+    palette: Kirigami.Theme.inherit ? Kirigami.Theme.palette : undefined
     Kirigami.Theme.colorSet: Kirigami.Theme.Button
     Kirigami.Theme.inherit: false
 

org.kde.desktop/ComboBox.qml
@@ -18,6 +18,7 @@ import "private" as Private
 T.ComboBox {
     id: controlRoot
 
+    palette: Kirigami.Theme.inherit ? Kirigami.Theme.palette : undefined
     Kirigami.Theme.colorSet: editable ? Kirigami.Theme.View : Kirigami.Theme.Button
     Kirigami.Theme.inherit: false
 

org.kde.desktop/DelayButton.qml
@@ -32,6 +32,7 @@ T.DelayButton {
         onActivated: controlRoot.clicked()
     }
 
+    palette: Kirigami.Theme.inherit ? Kirigami.Theme.palette : undefined
     Kirigami.Theme.colorSet: Kirigami.Theme.Button
     Kirigami.Theme.inherit: false
 

org.kde.desktop/Dial.qml
@@ -15,6 +15,7 @@ import org.kde.qqc2desktopstyle.private 1.0 as StylePrivate
 T.Dial {
     id: controlRoot
 
+    palette: Kirigami.Theme.palette
     implicitWidth: 128
     implicitHeight: 128
 

org.kde.desktop/ProgressBar.qml
@@ -14,6 +14,8 @@ import org.kde.kirigami 2.4 as Kirigami
 T.ProgressBar {
     id: controlRoot
 
+    palette: Kirigami.Theme.palette
+
     implicitWidth: 250
     implicitHeight: 22
 

org.kde.desktop/RoundButton.qml
@@ -15,6 +15,7 @@ import org.kde.kirigami 2.16 as Kirigami
 T.RoundButton {
     id: controlRoot
 
+    palette: Kirigami.Theme.inherit ? Kirigami.Theme.palette : undefined
     Kirigami.Theme.colorSet: !flat && (controlRoot.activeFocus || controlRoot.highlighted) ? Kirigami.Theme.Selection : Kirigami.Theme.Button
     Kirigami.Theme.inherit: flat && !down && !checked
 

org.kde.desktop/ScrollBar.qml
@@ -15,6 +15,8 @@ import org.kde.kirigami 2.20 as Kirigami
 T.ScrollBar {
     id: controlRoot
 
+    palette: Kirigami.Theme.palette
+
     implicitWidth: background.implicitWidth
     implicitHeight: background.implicitHeight
 

org.kde.desktop/SpinBox.qml
@@ -14,6 +14,8 @@ import org.kde.qqc2desktopstyle.private 1.0 as StylePrivate
 
 T.SpinBox {
     id: controlRoot
+
+    palette: Kirigami.Theme.inherit ? Kirigami.Theme.palette : undefined
     Kirigami.Theme.colorSet: Kirigami.Theme.View
     Kirigami.Theme.inherit: false
 

org.kde.desktop/TabBar.qml
@@ -15,6 +15,7 @@ import QtQuick.Templates 2.15 as T
 T.TabBar {
     id: controlRoot
 
+    palette: Kirigami.Theme.inherit ? Kirigami.Theme.palette : undefined
     Kirigami.Theme.colorSet: Kirigami.Theme.Window
     Kirigami.Theme.inherit: false
 

org.kde.desktop/TabButton.qml
@@ -18,6 +18,10 @@ import org.kde.kirigami 2.4 as Kirigami
 T.TabButton {
     id: controlRoot
 
+    palette: Kirigami.Theme.inherit ? Kirigami.Theme.palette : undefined
+    Kirigami.Theme.colorSet: Kirigami.Theme.Button
+    Kirigami.Theme.inherit: false
+
     //Some qstyles like fusion don't have correct pixel metrics here and just return 0
     implicitWidth: Math.max(styleitem.implicitWidth, textMetrics.width + Kirigami.Units.gridUnit * 2)
     implicitHeight: styleitem.implicitHeight || Kirigami.Units.gridUnit * 2

org.kde.desktop/TextArea.qml
@@ -17,6 +17,7 @@ import "private" as Private
 
 T.TextArea {
     id: controlRoot
+    palette: Kirigami.Theme.inherit ? Kirigami.Theme.palette : undefined
     Kirigami.Theme.colorSet: Kirigami.Theme.View
     Kirigami.Theme.inherit: false
 

org.kde.desktop/TextField.qml
@@ -16,6 +16,7 @@ import "private" as Private
 
 T.TextField {
     id: controlRoot
+    palette: Kirigami.Theme.inherit ? Kirigami.Theme.palette : undefined
     Kirigami.Theme.colorSet: Kirigami.Theme.View
     Kirigami.Theme.inherit: false
 

org.kde.desktop/ToolButton.qml
@@ -15,6 +15,7 @@ import org.kde.qqc2desktopstyle.private 1.0 as StylePrivate
 T.ToolButton {
     id: controlRoot
 
+    palette: Kirigami.Theme.inherit ? Kirigami.Theme.palette : undefined
     Kirigami.Theme.colorSet: flat ? Kirigami.Theme.Window : Kirigami.Theme.Button
     Kirigami.Theme.inherit: flat
 

org.kde.desktop/ToolTip.qml
@@ -15,6 +15,7 @@ import org.kde.kirigami 2.12 as Kirigami
 T.ToolTip {
     id: control
 
+    palette: Kirigami.Theme.inherit ? Kirigami.Theme.palette : undefined
     Kirigami.Theme.colorSet: Kirigami.Theme.Tooltip
     Kirigami.Theme.inherit: false
 