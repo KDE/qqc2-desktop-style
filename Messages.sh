da66b74ff76272d75c7dafc342e977de8562aa2b
Heiko Becker <mail@heiko-becker.de>
2024-05-19 22:27:00 +0200

    Extract translations into qqc2desktopstyle_qt5.pot
    ...to make it co-installable with the Qt6/KF6 version.

--- Messages.sh
+++ Messages.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/env bash
-$EXTRACT_TR_STRINGS `find . -name \*.qml -o -name \*.cpp` -o  $podir/qqc2desktopstyle_qt.pot
+$EXTRACT_TR_STRINGS `find . -name \*.qml -o -name \*.cpp` -o  $podir/qqc2desktopstyle_qt5.pot
