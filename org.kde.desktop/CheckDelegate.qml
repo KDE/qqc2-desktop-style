KDE/qqc2-desktop-style

commit 16f768b4292e3b0c128c8d5906dfa6985aa73b81

Support display property for delegates
Currently the delegates ignore the `display` property if you set it, and
instead always display everything in a row layout with both icon and
text.

This commit fixes that by adding support for the property, allowing
all of the delegate types to be laid out as expected.

org.kde.desktop/CheckDelegate.qml
@@ -2,6 +2,7 @@
     SPDX-FileCopyrightText: 2017 Marco Martin <mart@kde.org>
     SPDX-FileCopyrightText: 2017 The Qt Company Ltd.
     SPDX-FileCopyrightText: 2023 ivan tkachenko <me@ratijas.tk>
+    SPDX-FileCopyrightText: 2025 Nate Graham <nate@kde.org>
 
     SPDX-License-Identifier: LGPL-3.0-only OR GPL-2.0-or-later
 */
@@ -27,55 +28,64 @@ T.CheckDelegate {
     spacing: Kirigami.Units.smallSpacing
     padding: Kirigami.Settings.tabletMode ? Kirigami.Units.largeSpacing : Kirigami.Units.mediumSpacing
     horizontalPadding: Kirigami.Units.smallSpacing * 2
-    leftPadding: !mirrored ? horizontalPadding + implicitIndicatorWidth + spacing : horizontalPadding
-    rightPadding: mirrored ? horizontalPadding + implicitIndicatorWidth + spacing : horizontalPadding
+    leftPadding: !mirrored ? horizontalPadding + (controlRoot.display === T.AbstractButton.TextUnderIcon ? 0 : implicitIndicatorWidth) + spacing : horizontalPadding
+    rightPadding: mirrored ? horizontalPadding + (controlRoot.display === T.AbstractButton.TextUnderIcon ? 0 : implicitIndicatorWidth) + spacing : horizontalPadding
 
-    icon.width: Kirigami.Units.iconSizes.smallMedium
-    icon.height: Kirigami.Units.iconSizes.smallMedium
+    readonly property int __iconSize: controlRoot.display === T.AbstractButton.TextUnderIcon ? Kirigami.Units.iconSizes.medium : Kirigami.Units.iconSizes.smallMedium
+    icon.width: __iconSize
+    icon.height:__iconSize
 
-    T.ToolTip.visible: (Kirigami.Settings.tabletMode ? down : hovered) && (contentItem.truncated ?? false)
+    T.ToolTip.visible: (Kirigami.Settings.tabletMode ? down : hovered) && (textLabel.truncated ?? false)
     T.ToolTip.text: text
     T.ToolTip.delay: Kirigami.Units.toolTipDelay
 
     leftInset: TableView.view ? 0 : horizontalPadding / 2
     rightInset: TableView.view ? 0 : horizontalPadding / 2
+    // We want total spacing between consecutive list items to be
+    // verticalPadding. So use half that as top/bottom margin, separately
+    // ceiling/flooring them so that the total spacing is preserved.
     topInset: TableView.view ? 0 : Math.ceil(verticalPadding / 2)
     bottomInset: TableView.view ? 0 : Math.ceil(verticalPadding / 2)
 
-    contentItem: RowLayout {
+    contentItem: GridLayout {
         LayoutMirroring.enabled: controlRoot.mirrored
-        spacing: controlRoot.spacing
+        rows: controlRoot.display === T.AbstractButton.TextUnderIcon ? 2 : 1
+        columns: controlRoot.display === T.AbstractButton.TextBesideIcon ? 2 : 1
+        rowSpacing: controlRoot.spacing
+        columnSpacing: controlRoot.spacing
 
         property alias truncated: textLabel.truncated
 
         Kirigami.Icon {
-            Layout.alignment: Qt.AlignVCenter
-            visible: controlRoot.icon.name !== "" || controlRoot.icon.source.toString() !== ""
-            source: controlRoot.icon.name !== "" ? controlRoot.icon.name : controlRoot.icon.source
             Layout.preferredHeight: controlRoot.icon.height
             Layout.preferredWidth: controlRoot.icon.width
+            Layout.alignment: Qt.AlignVCenter | Qt.AlignHCenter
+            visible: controlRoot.display !== T.AbstractButton.TextOnly
+                && (controlRoot.icon.name !== "" || controlRoot.icon.source.toString() !== "")
+            source: controlRoot.icon.name !== "" ? controlRoot.icon.name : controlRoot.icon.source
         }
 
         Label {
             id: textLabel
 
-            Layout.alignment: Qt.AlignLeft
             Layout.fillWidth: true
+            Layout.fillHeight: true
 
             text: controlRoot.text
             font: controlRoot.font
             color: (((controlRoot.pressed && !controlRoot.checked) || controlRoot.highlighted) && !controlRoot.sectionDelegate) ? Kirigami.Theme.highlightedTextColor : Kirigami.Theme.textColor
             elide: Text.ElideRight
-            visible: controlRoot.text
-            horizontalAlignment: Text.AlignLeft
-            verticalAlignment: Text.AlignVCenter
+            visible: controlRoot.display !== T.AbstractButton.IconOnly && controlRoot.text
+            wrapMode: controlRoot.display === T.AbstractButton.TextUnderIcon ? Text.Wrap : Text.NoWrap
+            horizontalAlignment: controlRoot.display === T.AbstractButton.TextUnderIcon ? Text.AlignHCenter : Text.AlignLeft
+            verticalAlignment: controlRoot.display === T.AbstractButton.TextUnderIcon ? Text.AlignTop : Text.AlignVCenter
         }
     }
 
     indicator: Private.CheckIndicator {
         elementType: "checkbox"
         x: !controlRoot.mirrored ? controlRoot.horizontalPadding : controlRoot.width - width - controlRoot.horizontalPadding
-        y: controlRoot.topPadding + (controlRoot.availableHeight - height) / 2
+        y: controlRoot.topPadding + (controlRoot.display === T.AbstractButton.TextUnderIcon ? 0 : ((controlRoot.availableHeight - height) / 2) )
 
         control: controlRoot
         drawIcon: false

org.kde.desktop/ItemDelegate.qml
@@ -2,9 +2,11 @@
     SPDX-FileCopyrightText: 2017 Marco Martin <mart@kde.org>
     SPDX-FileCopyrightText: 2017 The Qt Company Ltd.
     SPDX-FileCopyrightText: 2023 ivan tkachenko <me@ratijas.tk>
+    SPDX-FileCopyrightText: 2025 Nate Graham <nate@kde.org>
 
     SPDX-License-Identifier: LGPL-3.0-only OR GPL-2.0-or-later
 */
+
 
 import QtQuick
 import QtQuick.Layouts
@@ -26,13 +28,14 @@ T.ItemDelegate {
     spacing: Kirigami.Units.smallSpacing
     padding: Kirigami.Settings.tabletMode ? Kirigami.Units.largeSpacing : Kirigami.Units.mediumSpacing
     horizontalPadding: Kirigami.Units.smallSpacing * 2
-    leftPadding: !mirrored ? horizontalPadding + (indicator ? implicitIndicatorWidth + spacing : 0) : horizontalPadding
-    rightPadding: mirrored ? horizontalPadding + (indicator ? implicitIndicatorWidth + spacing : 0) : horizontalPadding
+    leftPadding: !mirrored ? horizontalPadding + (indicator ? (controlRoot.display === T.AbstractButton.TextUnderIcon ? 0 : implicitIndicatorWidth) + spacing : 0) : horizontalPadding
+    rightPadding: mirrored ? horizontalPadding + (indicator ? (controlRoot.display === T.AbstractButton.TextUnderIcon ? 0 : implicitIndicatorWidth) + spacing : 0) : horizontalPadding
 
-    icon.width: Kirigami.Units.iconSizes.smallMedium
-    icon.height: Kirigami.Units.iconSizes.smallMedium
+    readonly property int __iconSize: controlRoot.display === T.AbstractButton.TextUnderIcon ? Kirigami.Units.iconSizes.medium : Kirigami.Units.iconSizes.smallMedium
+    icon.width: __iconSize
+    icon.height:__iconSize
 
-    T.ToolTip.visible: (Kirigami.Settings.tabletMode ? down : hovered) && (contentItem.truncated ?? false)
+    T.ToolTip.visible: (Kirigami.Settings.tabletMode ? down : hovered) && (textLabel.truncated ?? false)
     T.ToolTip.text: text
     T.ToolTip.delay: Kirigami.Units.toolTipDelay
 
@@ -44,26 +47,30 @@ T.ItemDelegate {
     topInset: TableView.view ? 0 : Math.ceil(verticalPadding / 2)
     bottomInset: TableView.view ? 0 : Math.ceil(verticalPadding / 2)
 
-    contentItem: RowLayout {
+    contentItem: GridLayout {
         LayoutMirroring.enabled: controlRoot.mirrored
-        spacing: controlRoot.spacing
+        rows: controlRoot.display === T.AbstractButton.TextUnderIcon ? 2 : 1
+        columns: controlRoot.display === T.AbstractButton.TextBesideIcon ? 2 : 1
+        rowSpacing: controlRoot.spacing
+        columnSpacing: controlRoot.spacing
 
         property alias truncated: textLabel.truncated
 
         Kirigami.Icon {
             selected: controlRoot.highlighted || controlRoot.down
-            Layout.alignment: Qt.AlignVCenter
-            visible: controlRoot.icon.name.length > 0 || controlRoot.icon.source.toString().length > 0
-            source: controlRoot.icon.name.length > 0 ? controlRoot.icon.name : controlRoot.icon.source
             Layout.preferredHeight: controlRoot.icon.height
             Layout.preferredWidth: controlRoot.icon.width
+            Layout.alignment: Qt.AlignVCenter | Qt.AlignHCenter
+            visible: controlRoot.display !== T.AbstractButton.TextOnly
+                && (controlRoot.icon.name.length > 0 || controlRoot.icon.source.toString().length > 0)
+            source: controlRoot.icon.name.length > 0 ? controlRoot.icon.name : controlRoot.icon.source
         }
 
         Label {
             id: textLabel
 
-            Layout.alignment: Qt.AlignLeft
             Layout.fillWidth: true
+            Layout.fillHeight: true
 
             text: controlRoot.text
             font: controlRoot.font
@@ -72,9 +79,10 @@ T.ItemDelegate {
                 : (controlRoot.enabled ? Kirigami.Theme.textColor : Kirigami.Theme.disabledTextColor)
 
             elide: Text.ElideRight
-            visible: controlRoot.text
-            horizontalAlignment: Text.AlignLeft
-            verticalAlignment: Text.AlignVCenter
+            visible: controlRoot.display !== T.AbstractButton.IconOnly && controlRoot.text
+            wrapMode: controlRoot.display === T.AbstractButton.TextUnderIcon ? Text.Wrap : Text.NoWrap
+            horizontalAlignment: controlRoot.display === T.AbstractButton.TextUnderIcon ? Text.AlignHCenter : Text.AlignLeft
+            verticalAlignment: controlRoot.display === T.AbstractButton.TextUnderIcon ? Text.AlignTop : Text.AlignVCenter
         }
     }
 

org.kde.desktop/RadioDelegate.qml
@@ -2,6 +2,7 @@
     SPDX-FileCopyrightText: 2017 Marco Martin <mart@kde.org>
     SPDX-FileCopyrightText: 2017 The Qt Company Ltd.
     SPDX-FileCopyrightText: 2023 ivan tkachenko <me@ratijas.tk>
+    SPDX-FileCopyrightText: 2025 Nate Graham <nate@kde.org>
 
     SPDX-License-Identifier: LGPL-3.0-only OR GPL-2.0-or-later
 */
@@ -27,55 +28,65 @@ T.RadioDelegate {
     spacing: Kirigami.Units.smallSpacing
     padding: Kirigami.Settings.tabletMode ? Kirigami.Units.largeSpacing : Kirigami.Units.mediumSpacing
     horizontalPadding: Kirigami.Units.smallSpacing * 2
-    leftPadding: !mirrored ? horizontalPadding + implicitIndicatorWidth + spacing : horizontalPadding
-    rightPadding: mirrored ? horizontalPadding + implicitIndicatorWidth + spacing : horizontalPadding
+    leftPadding: !mirrored ? horizontalPadding + (controlRoot.display === T.AbstractButton.TextUnderIcon ? 0 : implicitIndicatorWidth) + spacing : horizontalPadding
+    rightPadding: mirrored ? horizontalPadding + (controlRoot.display === T.AbstractButton.TextUnderIcon ? 0 : implicitIndicatorWidth) + spacing : horizontalPadding
 
-    icon.width: Kirigami.Units.iconSizes.smallMedium
-    icon.height: Kirigami.Units.iconSizes.smallMedium
+    readonly property int __iconSize: controlRoot.display === T.AbstractButton.TextUnderIcon ? Kirigami.Units.iconSizes.medium : Kirigami.Units.iconSizes.smallMedium
+    icon.width: __iconSize
+    icon.height:__iconSize
 
-    T.ToolTip.visible: (Kirigami.Settings.tabletMode ? down : hovered) && (contentItem.truncated ?? false)
+    T.ToolTip.visible: (Kirigami.Settings.tabletMode ? down : hovered) && (textLabel.truncated ?? false)
     T.ToolTip.text: text
     T.ToolTip.delay: Kirigami.Units.toolTipDelay
 
     leftInset: TableView.view ? 0 : horizontalPadding / 2
     rightInset: TableView.view ? 0 : horizontalPadding / 2
+    // We want total spacing between consecutive list items to be
+    // verticalPadding. So use half that as top/bottom margin, separately
+    // ceiling/flooring them so that the total spacing is preserved.
     topInset: TableView.view ? 0 : Math.ceil(verticalPadding / 2)
     bottomInset: TableView.view ? 0 : Math.ceil(verticalPadding / 2)
 
-    contentItem: RowLayout {
+    contentItem: GridLayout {
         LayoutMirroring.enabled: controlRoot.mirrored
-        spacing: controlRoot.spacing
+        rows: controlRoot.display === T.AbstractButton.TextUnderIcon ? 2 : 1
+        columns: controlRoot.display === T.AbstractButton.TextBesideIcon ? 2 : 1
+        rowSpacing: controlRoot.spacing
+        columnSpacing: controlRoot.spacing
 
         property alias truncated: textLabel.truncated
 
         Kirigami.Icon {
-            Layout.alignment: Qt.AlignVCenter
-            visible: controlRoot.icon.name !== "" || controlRoot.icon.source.toString() !== ""
-            source: controlRoot.icon.name !== "" ? controlRoot.icon.name : controlRoot.icon.source
             Layout.preferredHeight: controlRoot.icon.height
             Layout.preferredWidth: controlRoot.icon.width
+            Layout.alignment: Qt.AlignVCenter | Qt.AlignHCenter
+            visible: controlRoot.display !== T.AbstractButton.TextOnly
+                && (controlRoot.icon.name !== "" || controlRoot.icon.source.toString() !== "")
+            source: controlRoot.icon.name !== "" ? controlRoot.icon.name : controlRoot.icon.source
         }
 
         Label {
             id: textLabel
 
-            Layout.alignment: Qt.AlignLeft
             Layout.fillWidth: true
+            Layout.fillHeight: true
 
             text: controlRoot.text
             font: controlRoot.font
             color: (controlRoot.pressed && !controlRoot.checked && !controlRoot.sectionDelegate) ? Kirigami.Theme.highlightedTextColor : Kirigami.Theme.textColor
             elide: Text.ElideRight
-            visible: controlRoot.text
-            horizontalAlignment: Text.AlignLeft
-            verticalAlignment: Text.AlignVCenter
+            visible: controlRoot.display !== T.AbstractButton.IconOnly && controlRoot.text
+            wrapMode: controlRoot.display === T.AbstractButton.TextUnderIcon ? Text.Wrap : Text.NoWrap
+            horizontalAlignment: controlRoot.display === T.AbstractButton.TextUnderIcon ? Text.AlignHCenter : Text.AlignLeft
+            verticalAlignment: controlRoot.display === T.AbstractButton.TextUnderIcon ? Text.AlignTop : Text.AlignVCenter
         }
     }
 
     indicator: Private.CheckIndicator {
         elementType: "radiobutton"
         x: !controlRoot.mirrored ? controlRoot.horizontalPadding : controlRoot.width - width - controlRoot.horizontalPadding
-        y: controlRoot.topPadding + (controlRoot.availableHeight - height) / 2
+        y: controlRoot.topPadding + (controlRoot.display === T.AbstractButton.TextUnderIcon ? 0 : ((controlRoot.availableHeight - height) / 2) )
+
         control: controlRoot
         drawIcon: false
     }

org.kde.desktop/SwitchDelegate.qml
@@ -1,6 +1,7 @@
 /*
     SPDX-FileCopyrightText: 2017 Marco Martin <mart@kde.org>
     SPDX-FileCopyrightText: 2017 The Qt Company Ltd.
+    SPDX-FileCopyrightText: 2025 Nate Graham <nate@kde.org>
 
     SPDX-License-Identifier: LGPL-3.0-only OR GPL-2.0-or-later
 */
@@ -26,33 +27,41 @@ T.SwitchDelegate {
     spacing: Kirigami.Units.smallSpacing
     padding: Kirigami.Settings.tabletMode ? Kirigami.Units.largeSpacing : Kirigami.Units.mediumSpacing
     horizontalPadding: Kirigami.Units.smallSpacing * 2
-    leftPadding: !mirrored ? horizontalPadding + implicitIndicatorWidth + spacing : horizontalPadding
-    rightPadding: mirrored ? horizontalPadding + implicitIndicatorWidth + spacing : horizontalPadding
+    leftPadding: !mirrored ? horizontalPadding + (controlRoot.display === T.AbstractButton.TextUnderIcon ? 0 : implicitIndicatorWidth) + spacing : horizontalPadding
+    rightPadding: mirrored ? horizontalPadding + (controlRoot.display === T.AbstractButton.TextUnderIcon ? 0 : implicitIndicatorWidth) + spacing : horizontalPadding
 
-    icon.width: Kirigami.Units.iconSizes.smallMedium
-    icon.height: Kirigami.Units.iconSizes.smallMedium
+    readonly property int __iconSize: controlRoot.display === T.AbstractButton.TextUnderIcon ? Kirigami.Units.iconSizes.medium : Kirigami.Units.iconSizes.smallMedium
+    icon.width: __iconSize
+    icon.height:__iconSize
 
-    T.ToolTip.visible: (Kirigami.Settings.tabletMode ? down : hovered) && (contentItem.truncated ?? false)
+    T.ToolTip.visible: (Kirigami.Settings.tabletMode ? down : hovered) && (textLabel.truncated ?? false)
     T.ToolTip.text: text
     T.ToolTip.delay: Kirigami.Units.toolTipDelay
 
     leftInset: TableView.view ? 0 : horizontalPadding / 2
     rightInset: TableView.view ? 0 : horizontalPadding / 2
+    // We want total spacing between consecutive list items to be
+    // verticalPadding. So use half that as top/bottom margin, separately
+    // ceiling/flooring them so that the total spacing is preserved.
     topInset: TableView.view ? 0 : Math.ceil(verticalPadding / 2)
     bottomInset: TableView.view ? 0 : Math.ceil(verticalPadding / 2)
 
-    contentItem: RowLayout {
+    contentItem: GridLayout {
         LayoutMirroring.enabled: controlRoot.mirrored
-        spacing: controlRoot.spacing
+        rows: controlRoot.display === T.AbstractButton.TextUnderIcon ? 2 : 1
+        columns: controlRoot.display === T.AbstractButton.TextBesideIcon ? 2 : 1
+        rowSpacing: controlRoot.spacing
+        columnSpacing: controlRoot.spacing
 
         property alias truncated: textLabel.truncated
 
         Kirigami.Icon {
-            Layout.alignment: Qt.AlignVCenter
-            visible: controlRoot.icon.name !== "" || controlRoot.icon.source.toString() !== ""
-            source: controlRoot.icon.name !== "" ? controlRoot.icon.name : controlRoot.icon.source
             Layout.preferredHeight: controlRoot.icon.height
             Layout.preferredWidth: controlRoot.icon.width
+            Layout.alignment: Qt.AlignVCenter | Qt.AlignHCenter
+            visible: controlRoot.display !== T.AbstractButton.TextOnly
+                && (controlRoot.icon.name !== "" || controlRoot.icon.source.toString() !== "")
+            source: controlRoot.icon.name !== "" ? controlRoot.icon.name : controlRoot.icon.source
         }
 
         Label {
@@ -65,15 +74,17 @@ T.SwitchDelegate {
             font: controlRoot.font
             color: (controlRoot.pressed && !controlRoot.checked && !controlRoot.sectionDelegate) ? Kirigami.Theme.highlightedTextColor : Kirigami.Theme.textColor
             elide: Text.ElideRight
-            visible: controlRoot.text
-            horizontalAlignment: Text.AlignLeft
-            verticalAlignment: Text.AlignVCenter
+            visible: controlRoot.display !== T.AbstractButton.IconOnly && controlRoot.text
+            wrapMode: controlRoot.display === T.AbstractButton.TextUnderIcon ? Text.Wrap : Text.NoWrap
+            horizontalAlignment: controlRoot.display === T.AbstractButton.TextUnderIcon ? Text.AlignHCenter : Text.AlignLeft
+            verticalAlignment: controlRoot.display === T.AbstractButton.TextUnderIcon ? Text.AlignTop : Text.AlignVCenter
         }
     }
 
     indicator: Private.SwitchIndicator {
         x: !controlRoot.mirrored ? controlRoot.horizontalPadding : controlRoot.width - width - controlRoot.horizontalPadding
-        y: controlRoot.topPadding + (controlRoot.availableHeight - height) / 2
+        y: controlRoot.topPadding + (controlRoot.display === T.AbstractButton.TextUnderIcon ? 0 : ((controlRoot.availableHeight - height) / 2) )
+
         control: controlRoot
     }
 

tests/testDelegates.qml
@@ -1,0 +1,156 @@
+/*
+    SPDX-FileCopyrightText: 2025 Nate Graham <nate@kde.org>
+
+    SPDX-License-Identifier: LGPL-3.0-only OR GPL-2.0-or-later
+*/
+
+import QtQuick
+import QtQuick.Layouts
+import QtQuick.Templates as T
+import QtQuick.Controls as QQC
+
+QQC.ApplicationWindow {
+    id: root
+
+    readonly property int spacing: 18
+
+    visible: true
+
+    width: 800
+    height: 600
+
+    // List delegates
+    GridLayout {
+        anchors.fill: parent
+        anchors.margins: root.spacing
+
+        columns: 4
+        rowSpacing: root.spacing
+        columnSpacing: root.spacing
+
+
+        // ItemDelegates
+        Repeater {
+            model: [
+                T.AbstractButton.TextBesideIcon,
+                T.AbstractButton.TextUnderIcon,
+                T.AbstractButton.TextOnly,
+                T.AbstractButton.IconOnly
+            ]
+            delegate: QQC.ScrollView {
+                id: itemDelegateScrollview
+                property int display: modelData
+
+                Layout.fillWidth: true
+                Layout.fillHeight: true
+                contentItem: ListView {
+                    clip: true
+                    model: 4
+                    delegate: QQC.ItemDelegate {
+                        display: itemDelegateScrollview.display
+                        width: ListView.view.width
+                        icon.name: "edit-bomb"
+                        text: "Text label"
+                    }
+                }
+                Component.onCompleted: {
+                    background.visible = true
+                }
+            }
+        }
+
+
+        // CheckDelegates
+        Repeater {
+            model: [
+                T.AbstractButton.TextBesideIcon,
+                T.AbstractButton.TextUnderIcon,
+                T.AbstractButton.TextOnly,
+                T.AbstractButton.IconOnly
+            ]
+            delegate: QQC.ScrollView {
+                id: checkDelegateScrollview
+                property int display: modelData
+
+                Layout.fillWidth: true
+                Layout.fillHeight: true
+                contentItem: ListView {
+                    clip: true
+                    model: 4
+                    delegate: QQC.CheckDelegate {
+                        display: checkDelegateScrollview.display
+                        width: ListView.view.width
+                        icon.name: "edit-bomb"
+                        text: "Text label"
+                    }
+                }
+                Component.onCompleted: {
+                    background.visible = true
+                }
+            }
+        }
+
+
+        // RadioDelegates
+        Repeater {
+            model: [
+                T.AbstractButton.TextBesideIcon,
+                T.AbstractButton.TextUnderIcon,
+                T.AbstractButton.TextOnly,
+                T.AbstractButton.IconOnly
+            ]
+            delegate: QQC.ScrollView {
+                id: radioDelegateScrollview
+                property int display: modelData
+
+                Layout.fillWidth: true
+                Layout.fillHeight: true
+                contentItem: ListView {
+                    clip: true
+                    model: 4
+                    delegate: QQC.RadioDelegate {
+                        display: radioDelegateScrollview.display
+                        width: ListView.view.width
+                        icon.name: "edit-bomb"
+                        text: "Text label"
+                    }
+                }
+                Component.onCompleted: {
+                    background.visible = true
+                }
+            }
+        }
+
+
+        // SwitchDelegates
+        Repeater {
+            model: [
+                T.AbstractButton.TextBesideIcon,
+                T.AbstractButton.TextUnderIcon,
+                T.AbstractButton.TextOnly,
+                T.AbstractButton.IconOnly
+            ]
+            delegate: QQC.ScrollView {
+                id: switchDelegateScrollview
+                property int display: modelData
+
+                Layout.fillWidth: true
+                Layout.fillHeight: true
+                contentItem: ListView {
+                    clip: true
+                    model: 4
+                    delegate: QQC.SwitchDelegate {
+                        display: switchDelegateScrollview.display
+                        width: ListView.view.width
+                        icon.name: "edit-bomb"
+                        text: "Text label"
+                    }
+                }
+                Component.onCompleted: {
+                    background.visible = true
+                }
+            }
+        }
+    }
+}
+

commit 0c0e8afdbcc7ab44e8aaf95880aa9ef67c489210

Check for text truncation correctly to fix tooltip visibility
This regressed in 16f768b4292e3b0c128c8d5906dfa6985aa73b81 where I
changed this to look for the label itself, but that might not be
assigned in time, breaking the binding. The older method was more
reliable.

BUG: 505745
FIXED-IN: 6.16

org.kde.desktop/CheckDelegate.qml
@@ -35,7 +35,7 @@ T.CheckDelegate {
     icon.width: __iconSize
     icon.height:__iconSize
 
-    T.ToolTip.visible: (Kirigami.Settings.tabletMode ? down : hovered) && (textLabel.truncated ?? false)
+    T.ToolTip.visible: (Kirigami.Settings.tabletMode ? down : hovered) && (contentItem.truncated ?? false)
     T.ToolTip.text: text
     T.ToolTip.delay: Kirigami.Units.toolTipDelay
 

org.kde.desktop/ItemDelegate.qml
@@ -35,7 +35,7 @@ T.ItemDelegate {
     icon.width: __iconSize
     icon.height:__iconSize
 
-    T.ToolTip.visible: (Kirigami.Settings.tabletMode ? down : hovered) && (textLabel.truncated ?? false)
+    T.ToolTip.visible: (Kirigami.Settings.tabletMode ? down : hovered) && (contentItem.truncated ?? false)
     T.ToolTip.text: action instanceof Kirigami.Action ? action.tooltip : text
     T.ToolTip.delay: Kirigami.Settings.tabletMode ? Qt.styleHints.mousePressAndHoldInterval : Kirigami.Units.toolTipDelay
 

org.kde.desktop/RadioDelegate.qml
@@ -35,7 +35,7 @@ T.RadioDelegate {
     icon.width: __iconSize
     icon.height:__iconSize
 
-    T.ToolTip.visible: (Kirigami.Settings.tabletMode ? down : hovered) && (textLabel.truncated ?? false)
+    T.ToolTip.visible: (Kirigami.Settings.tabletMode ? down : hovered) && (contentItem.truncated ?? false)
     T.ToolTip.text: text
     T.ToolTip.delay: Kirigami.Units.toolTipDelay
 

org.kde.desktop/SwitchDelegate.qml
@@ -34,7 +34,7 @@ T.SwitchDelegate {
     icon.width: __iconSize
     icon.height:__iconSize
 
-    T.ToolTip.visible: (Kirigami.Settings.tabletMode ? down : hovered) && (textLabel.truncated ?? false)
+    T.ToolTip.visible: (Kirigami.Settings.tabletMode ? down : hovered) && (contentItem.truncated ?? false)
     T.ToolTip.text: text
     T.ToolTip.delay: Kirigami.Units.toolTipDelay
 